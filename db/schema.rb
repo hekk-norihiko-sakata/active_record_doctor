1159419069cea52ee9e725adef66e88ddabeebfd
hekk-norihiko-sakata <norihiko.sakata@happyelements.co.jp>
2026-02-05 16:25:31 +0900

    Add bad table without index

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_05_072510) do
+  create_table "bad_tables", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
---
+++ test/models/bad_table_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BadTableTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
